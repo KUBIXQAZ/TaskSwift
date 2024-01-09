9bc80ce61584734321f0f37bbb8f37e482c93dfb
KUBIXQAZ <kubajozwik20027@gmail.com>
2024-01-09 19:11:30 +0100

    removed something idek

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,10 +1,18 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
 
 namespace TaskSwift;
 
 [Activity(Theme = "@style/Maui.SplashTheme", ScreenOrientation = ScreenOrientation.Portrait, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        Window.SetStatusBarColor(Color.FromHex("#121212").ToAndroid());
+        Window.SetNavigationBarColor(Color.FromHex("#1c1c1c").ToAndroid());
+
+        base.OnCreate(savedInstanceState);
+    }
 }
